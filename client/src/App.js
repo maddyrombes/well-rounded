85fed0d34ffefdd8bcaa5905f093f532b87e0817
Madeline Rombes <madelinerombes@gmail.com>
2019-05-04 12:55:01 -0400

    setting up auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+import decode from 'jwt-decode';
+import AuthForm from './components/AuthForm';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: null,
+      authForm: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+  render() {
   return (
     <div className="App">
+      <h1>Welcome to Well Balanced</h1>
+      <Route path="/register" render={() => (
+          <AuthForm
+            authFormTitle="Register"
+            handleSubmit={this.handleRegister}
+            handleChange={this.handleAuthChange}
+            authForm={this.state.authForm}
+          />
+        )} />
+        <Route path="/login" render={() => (
+          <AuthForm
+            authFormTitle="Login"
+            handleSubmit={this.handleLogin}
+            handleChange={this.handleAuthChange}
+            authForm={this.state.authForm}
+          />
+        )} />
     </div>
   );
-}
+}}
 
 export default App;
