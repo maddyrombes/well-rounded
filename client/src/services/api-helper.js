a974b9617749831e38e0d7e04a9acd7b6f9f47b6
Madeline Rombes <madelinerombes@gmail.com>
2019-05-08 11:15:39 -0400

    change baseurl

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3000"
+const baseUrl = "https://mighty-springs-48129.herokuapp.com/"
 
 export const showUserProfile = (id) => {
   return fetch(`${baseUrl}/users/${id}`)
