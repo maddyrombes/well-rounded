e9c89b3e788ed62e2b6b94d72200e77792af3ce6
Madeline Rombes <madelinerombes@gmail.com>
2019-05-09 11:02:34 -0400

    update deleteuser

--- client/src/components/DeleteUser.jsx
+++ client/src/components/DeleteUser.jsx
@@ -6,22 +6,29 @@ class EditDeleteUser extends Component {
   render() {
     return (
       <div>
-        <div className="delete-header">
-          <h1 className="delete-logo">well rounded</h1>
-          <Link to={`/users/${this.props.match.params.id}/edit_ratings`} className="delete-back-btn">GO BACK</Link>
+      <div className="delete-header">
+
+        <div className="userprofile-logo-div">
+          <button className="logo"></button>
+          <h1 className="userprofile-logo">well rounded</h1>
         </div>
-        <div className="delete">
-          <p className="delete-para">If you're sure you want to delete your profile,<br/> click the button below. <br/>This action cannot be undone.</p>
-          <button
-              className="delete-btn"
-              onClick={(e) => {
-                  e.preventDefault()
-                  this.props.deleteUser(this.props.match.params.id)
-                  this.props.history.push(`/`)
-              }}
-          >DELETE ACCOUNT</button>
-        </div>
+
+        <Link to={`/users/${this.props.match.params.id}/edit_ratings`} className="delete-back-btn">GO BACK</Link>
+
       </div>
+      
+      <div className="delete">
+        <p className="delete-para">If you're sure you want to delete your profile,<br/> click the button below. <br/>This action cannot be undone.</p>
+        <button
+            className="delete-btn"
+            onClick={(e) => {
+                e.preventDefault()
+                this.props.deleteUser(this.props.match.params.id)
+                this.props.history.push(`/`)
+            }}
+        >DELETE ACCOUNT</button>
+      </div>
+    </div>
     )
   }
 }
