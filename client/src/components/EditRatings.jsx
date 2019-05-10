7c3b6b1e76f789838e65d353ff77069960e8ab64
Madeline Rombes <madelinerombes@gmail.com>
2019-05-10 13:01:40 -0400

    range slider

--- client/src/components/EditRatings.jsx
+++ client/src/components/EditRatings.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
+import Slider from 'react-rangeslider'
+import 'react-rangeslider/lib/index.css'
 
 class EditRatings extends Component {
 
@@ -11,6 +13,7 @@ class EditRatings extends Component {
     const { currentUser } = this.props
     return (
         <div>
+            
         {currentUser && 
         <>
         <div className="edit-header">
@@ -31,6 +34,7 @@ class EditRatings extends Component {
         
         <p className="edit-prompt">How are you doing for each of these categories, on a scale of 1 to 10?</p>
 
+
         <form className="edit-form-div" onSubmit={(e) => {
             e.preventDefault()
             this.props.updateRatings()
@@ -38,13 +42,27 @@ class EditRatings extends Component {
         }
         }>
             <div>
+            <datalist id="tickmarks">
+                <option value="1" label="1">1</option>
+                <option value="2" label="2">2</option>
+                <option value="3" label="3">3</option>
+                <option value="4" label="4">4</option>
+                <option value="5" label="5">5</option>
+                <option value="6" label="6">6</option>
+                <option value="7" label="7">7</option>
+                <option value="8" label="8">8</option>
+                <option value="9" label="9">9</option>
+                <option value="10" label="10">10</option>
+            </datalist>
             <label>Finance</label>
             <input 
                 className="editform"
                 name="f_rating" 
-                type="number" 
+                type="range" 
                 min="1"
                 max="10"
+                list="tickmarks"
+                labels="datalist"
                 placeholder={currentUser.ratings[0].rating} 
                 value={this.props.ratingForm.f_rating}
                 onChange={this.props.handleUpdateForm} />
@@ -54,9 +72,11 @@ class EditRatings extends Component {
             <input 
                 className="editform"
                 name="ll_rating" 
-                type="number" 
+                type="range" 
                 min="1"
                 max="10"
+                list="tickmarks"
+                labels="datalist"
                 placeholder={currentUser.ratings[1].rating}
                 value={this.props.ratingForm.ll_rating}
                 onChange={this.props.handleUpdateForm} />
@@ -66,9 +86,11 @@ class EditRatings extends Component {
             <input 
                 className="editform"
                 name="ff_rating" 
-                type="number" 
+                type="range" 
                 min="1"
                 max="10"
+                list="tickmarks"
+                labels="datalist"
                 placeholder={currentUser.ratings[2].rating}
                 value={this.props.ratingForm.ff_rating}
                 onChange={this.props.handleUpdateForm} />
@@ -78,9 +100,11 @@ class EditRatings extends Component {
             <input 
                 className="editform"
                 name="w_rating" 
-                type="number" 
+                type="range" 
                 min="1"
                 max="10"
+                list="tickmarks"
+                labels="datalist"
                 placeholder={currentUser.ratings[3].rating}
                 value={this.props.ratingForm.w_rating}
                 onChange={this.props.handleUpdateForm} />
@@ -90,9 +114,11 @@ class EditRatings extends Component {
             <input 
                 className="editform"
                 name="c_rating" 
-                type="number" 
+                type="range" 
                 min="1"
                 max="10"
+                list="tickmarks"
+                labels="datalist"
                 placeholder={currentUser.ratings[4].rating}
                 value={this.props.ratingForm.c_rating}
                 onChange={this.props.handleUpdateForm} />
@@ -102,9 +128,11 @@ class EditRatings extends Component {
             <input 
                 className="editform"
                 name="e_rating" 
-                type="number" 
+                type="range" 
                 min="1"
                 max="10"
+                list="tickmarks"
+                labels="datalist"
                 placeholder={currentUser.ratings[5].rating}
                 value={this.props.ratingForm.e_rating}
                 onChange={this.props.handleUpdateForm} />
@@ -114,9 +142,11 @@ class EditRatings extends Component {
             <input
                 className="editform" 
                 name="he_rating" 
-                type="number" 
+                type="range" 
                 min="1"
                 max="10"
+                list="tickmarks"
+                labels="datalist"
                 placeholder={currentUser.ratings[6].rating}
                 value={this.props.ratingForm.he_rating}
                 onChange={this.props.handleUpdateForm} />
@@ -126,9 +156,11 @@ class EditRatings extends Component {
             <input 
                 className="editform"
                 name="ss_rating" 
-                type="number" 
+                type="range" 
                 min="1"
                 max="10"
+                list="tickmarks"
+                labels="datalist"
                 placeholder={currentUser.ratings[7].rating}
                 value={this.props.ratingForm.ss_rating}
                 onChange={this.props.handleUpdateForm} />
